41737ff20c012eed33bc84afc2545543e72136d9
Daniel Liden <djliden91@gmail.com>
2024-02-08 21:00:25 -0600

    apply autofmt

--- ollama.el
+++ ollama.el
@@ -40,30 +40,30 @@ MODEL and PROMPT are required. ARGS is a plist for optional parameters."
   ;; Construct the JSON payload with optional parameters included if they are provided
   (let
       ((generate-args
-         `(("model" . ,model)
-           ("prompt" . ,prompt) ,@
-           (when (plist-member args :system)
-             `(("system" . ,(plist-get args :system))))
-           ,@
-           (when (plist-member args :template)
-             `(("template" . ,(plist-get args :template))))
-           ,@
-           (when (plist-member args :context)
-             `(("context" . ,(plist-get args :context))))
-           ,@
-           (when (plist-member args :stream)
-             `(("stream" . ,(plist-get args :stream))))
-           ,@
-           (when (plist-member args :raw)
-             `(("raw" . ,(plist-get args :raw))))
-           ,@
-           (when (plist-member args :format)
-             `(("format" . ,(plist-get args :format))))
-           ,@
-           (when (plist-member args :options)
-             `(("options" . ,(plist-get args :options))))
-           ;; `stream` is hardcoded as :json-false based on your requirement
-           ("stream" . :json-false))))
+        `(("model" . ,model)
+          ("prompt" . ,prompt) ,@
+          (when (plist-member args :system)
+            `(("system" . ,(plist-get args :system))))
+          ,@
+          (when (plist-member args :template)
+            `(("template" . ,(plist-get args :template))))
+          ,@
+          (when (plist-member args :context)
+            `(("context" . ,(plist-get args :context))))
+          ,@
+          (when (plist-member args :stream)
+            `(("stream" . ,(plist-get args :stream))))
+          ,@
+          (when (plist-member args :raw)
+            `(("raw" . ,(plist-get args :raw))))
+          ,@
+          (when (plist-member args :format)
+            `(("format" . ,(plist-get args :format))))
+          ,@
+          (when (plist-member args :options)
+            `(("options" . ,(plist-get args :options))))
+          ;; `stream` is hardcoded as :json-false based on your requirement
+          ("stream" . :json-false))))
     ;; Send the request to the Ollama API
     (ollama-send-request
      "generate" generate-args 'simple-ollama-process-filter)))
@@ -96,6 +96,3 @@ MODEL and MESSAGES are required. ARGS is a plist for optional parameters."
     ;; Send the request to the Ollama API
     (ollama-send-request
      "chat" chat-args 'simple-ollama-process-filter)))
-
-
-
